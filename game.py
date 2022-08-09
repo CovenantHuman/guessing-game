751b2b4c7d36f743b977dc473f15cae8d0d810ff
Michaela Healton <mshealton@gmail.com>
2022-08-09 12:51:34 -0700

    Base game finished

--- game.py
+++ game.py
@@ -1,9 +1,11 @@
 """A number-guessing game."""
 
 import random
-# Put your code here
+tries = 0
+ 
+# ----- welcome / user input --------
 print("Howdy!")
-name = input("What's your name?")
+name = input("What's your name? ")
 print(f"Okay, {name}, we're thinking of a number between 1 and 100.")
 secret_number = random.randint(1,100)
 # for testing coding
@@ -11,12 +13,31 @@ print(f"The secret number is {secret_number}")
 
 game_over = False
 
-while not game_over:
-    guess = int(input("What do you think the number is?"))
+def check_guess(guess, secret_number):
+    global tries
     if guess > secret_number:
+        tries += 1
         print("Too high! Try again.")
     elif guess < secret_number:
+        tries += 1
         print("Too low! Try again")
     else:
         game_over = True
-        print(f"You got it! The secret number was {secret_number}")
+        tries += 1
+        print(f"You got it! The secret number was {secret_number}. You got it in {tries} guesses")
+        return game_over
+
+
+while not game_over:
+    try:
+        guess = int(input("What do you think the number is? "))
+        if guess >= 101 or guess <= 0:
+            tries += 1
+            print("You fool. Play the game correctly")
+        else: 
+            game_over = check_guess(guess, secret_number)
+    except ValueError:
+        tries += 1
+        print("You are ridiculous. Enter an integer.")
+
+        
