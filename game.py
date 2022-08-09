b5ba2f0f08a49173687a27441f769eb654676e72
Michaela Healton <mshealton@gmail.com>
2022-08-09 16:04:07 -0700

    Limit number of guesses

--- game.py
+++ game.py
@@ -9,7 +9,7 @@ name = input("Howdy! What's your name? ")
 
 while play_again: 
     tries = 0
-    print(f"Okay, {name}, we're thinking of a number between 1 and 100.")
+    print(f"Okay, {name}, we're thinking of a number between 1 and 100. You get 7 guesses.")
     secret_number = random.randint(1,100)
     # for testing coding
     print(f"The secret number is {secret_number}")
@@ -17,29 +17,33 @@ while play_again:
     game_over = False
 
     while not game_over:
-        guess = input("What do you think the number is? ")
-        tries += 1
-        try:
-            guess = int(guess)
-        except ValueError:
-            print("You are ridiculous. Enter an integer.")
-            continue
-            
-        if guess >= 101 or guess <= 0:
-                print("You fool. Play the game correctly")
-        else: 
-            if guess > secret_number:
-                print("Too high! Try again.")
-            elif guess < secret_number:
-                print("Too low! Try again")
-            else:
-                game_over = True
-                if best_score == None or best_score > tries:
-                    best_score = tries
-                print(f"You got it! The secret number was {secret_number}. You got it in {tries} guesses")
-                print(f"The current best score is: {best_score}")
-
+        if tries > 6:
+            print("You took too many guesses!")
+            break
+        else:
+            guess = input("What do you think the number is? ")
+            tries += 1
+            try:
+                guess = int(guess)
+            except ValueError:
+                print("You are ridiculous. Enter an integer.")
+                continue
                 
+            if guess >= 101 or guess <= 0:
+                    print("You fool. Play the game correctly")
+            else: 
+                if guess > secret_number:
+                    print("Too high! Try again.")
+                elif guess < secret_number:
+                    print("Too low! Try again")
+                else:
+                    game_over = True
+                    if best_score == None or best_score > tries:
+                        best_score = tries
+                    print(f"You got it! The secret number was {secret_number}. You got it in {tries} guesses")
+                    print(f"The current best score is: {best_score}")
+
+              
                 
     restart = input("Would you like to play again? Y/N ").lower()
     if restart == "n":
